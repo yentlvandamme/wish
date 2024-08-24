448d0e96ff54f976b67855a4471fe06054291c6f
yentlvandamme <yentl.van.damme@gmail.com>
2024-08-24 12:51:26 -0400

    Allow asking questions to Gemini and return the response

--- main.go
+++ main.go
@@ -13,10 +13,40 @@ import (
 )
 
 func main () {
+	if len(os.Args) < 2 {
+		fmt.Println("Pass a string value to act as your prompt")
+		os.Exit(1)
+	}
+
+	prompt := os.Args[1]
+
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 
+	createConfigIfNotExist()
+
+	viper.ReadInConfig()
+	apiKey := viper.GetString("api_key")
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer client.Close()
+	
+	model := client.GenerativeModel("gemini-1.5-flash")
+
+	resp, err := model.GenerateContent(ctx, genai.Text(prompt))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	printResponse(resp)	
+}
+
+func createConfigIfNotExist() {
 	if _, err := os.Stat("./config.yaml"); os.IsNotExist(err) {
 		_, err := os.OpenFile("./config.yaml", os.O_RDONLY|os.O_CREATE, 0666)
 		if err != nil {
@@ -35,25 +65,6 @@ func main () {
 			fmt.Println(err)
 		}
 	}
-
-	viper.ReadInConfig()
-	apiKey := viper.GetString("api_key")
-
-	ctx := context.Background()
-	client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey))
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer client.Close()
-	
-	model := client.GenerativeModel("gemini-1.5-flash")
-
-	resp, err := model.GenerateContent(ctx, genai.Text("How are you today?"))
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	printResponse(resp)	
 }
 
 func printResponse(resp *genai.GenerateContentResponse) {
